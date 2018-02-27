3efe96060d96e184e6944f54a3b201708f1d0caa
HuRunsheng <34936945+HuRunsheng@users.noreply.github.com>
2018-02-27 15:50:41 +0800

    update author

--- src/main/java/test1/EscapeTest.java
+++ src/main/java/test1/EscapeTest.java
@@ -2,6 +2,12 @@ package test1;
 
 import org.apache.commons.text.StringEscapeUtils;
 
+/**
+ * EscapeTest class
+ * 
+ * @author HuRunsheng
+ * @date 2018/02/27
+ */
 public class EscapeTest {
 
 	public static void main(String[] args) {
